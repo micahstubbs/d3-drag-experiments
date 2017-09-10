69bd87569ef43f04dfdbad4fa94bd83ec5c0efeb
micahstubbs <micah.stubbs@gmail.com>
2017-09-09 21:19:25 -0700

    add static canvas example

--- 03/vis.js
+++ 03/vis.js
@@ -1,6 +1,3 @@
-const w = 200;
-const h = 200;
-
 const drag = d3
   .drag()
   .on('start', () => {
@@ -15,35 +12,59 @@ const drag = d3
     } else d3.select(this).attr('transform', `translate(${x},${y})`);
   });
 
-const container = d3
-  .select('body')
+/* prettier-ignore */
+d3.select('body')
   .append('canvas')
   .attr('width', 960)
   .attr('height', 500);
 
-const group = container.append('g');
+const canvas = d3.select('canvas');
+const context = canvas.node().getContext('2d');
+const width = canvas.property('width');
+const height = canvas.property('height');
 
-const rect = group
-  .append('rect')
-  .attr('width', w)
-  .attr('height', h)
-  .attr('x', 10)
-  .attr('yx', 10);
+const circles = [{ x: 50, y: 50, radius: 10 }, { x: 100, y: 70, radius: 10 }];
 
-const circle1 = group
-  .append('circle')
-  .attr('cx', 50)
-  .attr('cy', 50)
-  .attr('r', 10)
-  .style('fill', 'red');
+function render() {
+  // draw the black rectangle
+  context.clearRect(0, 0, width, height);
+  context.fillRect(10, 10, 210, 210);
 
-const circle2 = group
-  .append('circle')
-  .attr('cx', 100)
-  .attr('cy', 70)
-  .attr('r', 10)
-  .style('fill', 'red');
+  // draw the red circles
+  circles.forEach(circle => {
+    context.beginPath();
+    context.moveTo(circle.x + circle.radius, circle.y);
+    context.arc(circle.x, circle.y, circle.radius, 0, 2 * Math.PI);
+    context.fillStyle = 'red';
+    context.fill();
+  });
+}
 
-circle1.call(drag);
-circle2.call(drag);
-group.call(drag);
+render();
+
+// const group = container.append('g');
+
+// const rect = group
+//   .append('rect')
+//   .attr('width', w)
+//   .attr('height', h)
+//   .attr('x', 10)
+//   .attr('yx', 10);
+
+// const circle1 = group
+//   .append('circle')
+//   .attr('cx', 50)
+//   .attr('cy', 50)
+//   .attr('r', 10)
+//   .style('fill', 'red');
+
+// const circle2 = group
+//   .append('circle')
+//   .attr('cx', 100)
+//   .attr('cy', 70)
+//   .attr('r', 10)
+//   .style('fill', 'red');
+
+// circle1.call(drag);
+// circle2.call(drag);
+// group.call(drag);
